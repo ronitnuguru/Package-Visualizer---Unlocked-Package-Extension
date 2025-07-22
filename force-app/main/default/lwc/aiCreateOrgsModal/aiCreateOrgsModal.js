949c02dcd613046a84175d6a1b1042bc81da0962
ronitnuguru <ronit.nuguru@gmail.com>
2025-07-21 18:08:17 -0700

    0.60

--- force-app/main/default/lwc/aiCreateOrgsModal/aiCreateOrgsModal.js
+++ force-app/main/default/lwc/aiCreateOrgsModal/aiCreateOrgsModal.js
@@ -9,8 +9,6 @@ export default class AiCreateOrgsModal extends LightningModal  {
     agentPrompt;
     parsedContent;
 
-
-
     connectedCallback(){
         this.parsedContent = JSON.parse(this.content);
 
